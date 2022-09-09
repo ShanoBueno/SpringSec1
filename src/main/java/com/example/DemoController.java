9e31f4b17ad60bab27eba416a59965017e6bf121
ShanoBueno <shane.messmer1@gmail.com>
2022-09-09 15:38:19 -0400

    pushes project

---
+++ src/main/java/com/example/DemoController.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+	@ResponseBody
+	@RequestMapping("/")
+	public String demo() {
+		return "hi simplilearn users"; 
+	}
+	
+	@ResponseBody
+	@RequestMapping("/user")
+	public String demouser() {
+		return "hi simplilearn role user"; 
+	}
+	
+	@ResponseBody
+	@RequestMapping("/admin")
+	public String demoadmin() {
+		return "hi simplilearn role admin"; 
+	}
+	
+	
+	
+}
